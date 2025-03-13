ecf10468413bed538184070868d16670e9248e30
akobi <201678915+atkobi007@users.noreply.github.com>
2025-03-13 21:45:07 +0700

    feat: 卡片几种模式适配处理

--- src/components/common/MovieItem.vue
+++ src/components/common/MovieItem.vue
@@ -1,21 +1,65 @@
 <script setup>
 
+const props = defineProps({
+  fitHeight: Boolean,//宽度100%(220px)
+  fitWidth: Boolean,//宽度100%
+  score: String,//评分
+  hot: String,//热门数
+  typeText: String,//类型
+})
+
+/** ### 设置高度 */
+const setItemCoverCls = computed(() => {
+  if (props.fitWidth) {
+    return ['item-cover-width-fill'];
+  }
+  if (props.fitHeight) {
+    return ['item-cover-fill']
+  }
+  return ['item-cover']
+})
+
+/** ### 设置高度 */
+const setItemCls = computed(() => {
+  if (props.fitWidth) {
+    return ['item-fill']
+  }
+  return ['item']
+})
+/** ### 设置标题和类型样式 */
+const setItemTitleType = computed(() => {
+  if (props.fitWidth) {
+    return ['item-title-type1'];
+  }
+  return ['item-title-type']
+})
+
+/** ### 设置类型样式 */
+const setItemType = computed(() => {
+  if (props.fitWidth) {
+    return ['item-type1'];
+  }
+  return ['item-type']
+})
+
 </script>
 
 <template>
-  <div class="item">
-    <div class="item-cover">
-      <img src="https://fastly.jsdelivr.net/npm/@vant/assets/apple-1.jpeg" width="100%" height="100%">
-      <div class="item-container">
-        <div>
+  <div class="item" :class="setItemCls">
+    <div class="item-cover border-radius-top" :class="setItemCoverCls">
+      <img class="border-radius-top" src="https://fastly.jsdelivr.net/npm/@vant/assets/apple-1.jpeg" width="100%" height="100%">
+      <div class="item-container border-radius-bottom">
+        <div class="content-height">
+          <div class="content-hot">{{ score }}</div>
           <van-icon name="fire" class="content-bold" style="margin-right: 5px;" />
-          <span class="content-bold">6058</span>
+          <span class="content-bold">{{ hot }}</span>
         </div>
         <span class="content-bold">00:00:42</span>
       </div>
     </div>
-    <div class="item-title van-ellipsis">
-      标题信息标题信息标题信息标题信息标题信息
+    <div :class="setItemTitleType">
+      <span class="item-title van-ellipsis">标题信息标题信息标题信息标题信息标题信息</span>
+      <span :class="setItemType">{{ typeText }}</span>
     </div>
   </div>
 </template>
@@ -24,11 +68,39 @@
 .item {
   width: 174px;
   height: auto;
+  border-radius: 16px;
+}
+
+.item-fill {
+  width: 100%;
+  height: auto;
+  border-radius: 16px;
 }
 
 .item-cover {
   width: 174px;
   height: 100px;
+  position: relative;
+  background-color: yellow;  
+}
+
+.border-radius-top{
+  border-radius: 4px 4px 0px 0px;
+}
+.border-radius-bottom{
+  border-radius: 0px 0px 4px 4px;
+}
+
+.item-cover-width-fill {
+  width: 100%;
+  height: 170px;
+  position: relative;
+  background-color: yellow;
+}
+
+.item-cover-fill {
+  width: 100%;
+  height: 220px;
   position: relative;
   background-color: yellow;
 }
@@ -43,10 +115,23 @@
   padding-left: 6px;
   padding-right: 6px;
 
+  /* border-radius: 0px 0px 6px 6px; */
+
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
+}
+
+.content-height {
+  height: 57px;
+}
+
+.content-hot {
+  color: #ff743d;
+  margin-bottom: 5px;
+  font-size: 16px;
+  font-weight: bold;
 }
 
 .content-bold {
@@ -55,10 +140,32 @@
 }
 
 .item-title {
-  width: 100%;
   height: 30px;
   line-height: 40px;
   font-size: 14px;
   color: white;
+}
+
+.item-type {
+  font-size: 12px;
+  color: gray;
+}
+
+.item-title-type {
+  display: flex;
+  flex-direction: column;
+}
+
+.item-title-type1 {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+
+.item-type1 {
+  font-size: 12px;
+  color: gray;
+  height: 30px;
+  line-height: 40px;
 }
 </style>
--- src/types/components.d.ts
+++ src/types/components.d.ts
@@ -5,7 +5,7 @@
 // biome-ignore lint: disable
 export {}
 
-
+/* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
     Cartoon: typeof import('./../components/tabs/Cartoon.vue')['default']
